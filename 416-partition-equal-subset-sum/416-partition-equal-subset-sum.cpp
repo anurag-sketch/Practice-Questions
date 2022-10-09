8540797fecaf0160103e8e9655b63922ce67845b
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2022-10-09 11:52:42 +0530

    Time: 172 ms (84.93%), Space: 18.5 MB (48.37%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool subset(vector<int> arr,int sum,int n)
+    {
+       
+        int t[n+1][sum+1];
+        
+        for(int i=0;i<(n+1);i++)
+        {
+          for(int j=0;j<(sum+1);j++)
+          {
+                 if(i==0)
+                 {
+                    t[i][j]=false;
+                 }
+              if(j==0)
+              {
+                  t[i][j]=true;
+              }
+          }
+        }
+        for(int i=1;i<(n+1);i++)
+        {
+           for(int j=1;j<(sum+1);j++)
+           {
+               if(arr[i-1]<=j)
+               {
+                   t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+               }
+               else
+               {
+                   t[i][j]=t[i-1][j];
+               }
+           }
+        }
+        return t[n][sum];
+        
+    }
+    
+    
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum=sum+nums[i];
+        }
+        if(sum%2!=0)
+        {
+             return false;
+        }
+        else
+        {
+            return subset(nums,(sum/2),(nums.size()));
+        }
+        
+    }
+};
