c75896717e341eb318b28867b1c19a652004911c
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2022-10-16 10:29:42 +0530

    Time: 3 ms (47.35%), Space: 6.2 MB (7.59%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+         
+        for(int i=0;i<s.size();i++)
+        {
+            if(int(s[i])>=65 && int(s[i])<=90)
+            {
+                s[i]=char(int(s[i])+32);
+            }
+        }
+    
+        return s;
+    
+        
+    }
+};
+        
