56acb8852365f1cb0def69d4b05a356e4d2ef094
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2022-10-24 17:05:27 +0530

    Time: 3 ms (99.97%), Space: 6 MB (31.24%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(n%3==0)
+        {
+            while(n%3==0)
+            {
+                n=n/3;
+            }
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
