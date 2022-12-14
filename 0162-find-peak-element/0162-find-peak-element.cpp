d8f26149c6403203c69ac2f4ef97c17727d5cf9c
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2022-12-14 13:41:47 +0530

    Time: 4 ms (87.35%), Space: 9 MB (20.24%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,43 +1,54 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int mx=INT_MIN;
-           for(int i=0;i<nums.size();i++)
-               {
-                   mx=max(nums[i],mx);
-               }
-           if(nums.size()<=2)
-           {
+        
+        int start=0;
+        int end = nums.size()-1;
+        if(nums.size() == 1 || nums.size() == 0)
+        {
+            return 0;
+        }
+        
+        while(start<=end)
+        {
+            int mid = start + (end - start)/2;
             
-               for(int i=0;i<nums.size();i++)
-               {
-                   if(nums[i]==mx)
-                   {
-                       return i;
-                   }
-               }
-           }
-
-
-        if(nums.size()>2)
-        {
-
-        for(int i=1;i<nums.size()-1;i++)
-        {
-            if((nums[i]>nums[i-1]) && (nums[i]>nums[i+1]))
+            if((mid > 0) && mid < (nums.size()-1))
             {
-                return i;
+                
+                if((nums[mid] > nums[mid-1]) && (nums[mid] > nums[mid+1]))
+                {
+                    return mid;
+                }
+                else if(nums[mid] < nums[mid -1])
+                {
+                    end = mid-1;
+                 
+                }
+                else if (nums[mid] < nums[mid+1])
+                {
+                   start=mid+1;
+                }
+            }
+            else if(mid == 0)
+            {
+                if( nums[0] > nums[1])
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                if(nums[nums.size() -1] > nums[nums.size() -2])
+                {
+                    return (nums.size() - 1);
+                }
             }
         }
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==mx)
-            {
-                return i;
-            }
-        }
-        }
-        return 0;
+return 0;
     }
 };
