864a6dd0161953410f506f4e5ec21f9397d1234d
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2022-12-28 23:15:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (38.01%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int ans = pow(x,0.5);
+        return ans;
+        
+//         vector<int> ans;
+        
+//         for(int i=1;i<2^15;i++)
+//         {
+//             ans.push_back(i*i);
+//         }
+        
+        
+        
+        
+    }
+};
