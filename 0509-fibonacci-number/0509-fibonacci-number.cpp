e92e8d349851d20f5d739f6950d4e2f3b6a23790
Anurag Sharma <64497662+anurag-sketch@users.noreply.github.com>
2023-02-07 23:38:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.34%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        vector<int> v(n+1);
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        v[0]=0;
+        v[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            v[i] = v[i-1] + v[i-2];
+        }
+        
+        return v[n];
+        
+    }
+};
